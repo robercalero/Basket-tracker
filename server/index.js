9d9bdb49a389c1d0b754105e1fe16f3e89df1693
Roberto Muñoz <robercalero2004@gmail.com>
2026-05-26 01:27:15 +0200

    fix: use Express-compatible wildcard '/(.*)' instead of '/*'

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.use('/api', aiRoutes);
 if (process.env.NODE_ENV === 'production') {
   const distPath = path.join(__dirname, '..', 'dist');
   app.use(express.static(distPath));
-  app.get('/*', (req, res) => {
+  app.get('/(.*)', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
